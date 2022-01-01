91442a7ce9cdf01032058fdf5397190073662a02
liszt21 <1832666492@qq.com>
2022-01-02 00:13:00 +0800

    feat(core): add features

--- src/core.lisp
+++ src/core.lisp
@@ -9,9 +9,17 @@
 (defun get-system-info ()
   "Get system revelent infos"
   ;; set features
+  (push :likit cl:*features*)
+
   (loop for distro in (run/lines "grep -Pho '[a-zA-Z ]*(?= Linux)' /etc/*-release | awk '!a[$0]++{print}'")
         for key = (intern (string-upcase distro) 'keyword)
-        do (push key cl:*features*)))
+        do (push key cl:*features*))
+
+  (loop for name in (run/lines (format nil "cat /etc/passwd | grep -P '~A:' | grep -Pho '[a-z]*$'" (UIOP:getenv "USER")))
+        for key = (intern (string-upcase name) 'keyword)
+        do (push key cl:*features*))
+
+  (when (equal "LISZT" (string-upcase (uiop:getenv "USER")))
+    (push :me cl:*features*)))
 
 (get-system-info)
-
