2261d74aff3b387081395d4cfc5f1614bdf9d157
liszt21 <1832666492@qq.com>
2022-01-02 17:44:15 +0800

    fix(core): exclude features in windows

--- src/core.lisp
+++ src/core.lisp
@@ -11,10 +11,12 @@
   ;; set features
   (push :likit cl:*features*)
 
+  #+os-unix
   (loop for distro in (run/lines "grep -Pho '[a-zA-Z ]*(?= Linux)' /etc/*-release | awk '!a[$0]++{print}'")
         for key = (intern (string-upcase distro) 'keyword)
         do (push key cl:*features*))
 
+  #-os-windows
   (loop for name in (run/lines (format nil "cat /etc/passwd | grep -P '~A:' | grep -Pho '[a-z]*$'" (UIOP:getenv "USER")))
         for key = (intern (string-upcase name) 'keyword)
         do (push key cl:*features*))
